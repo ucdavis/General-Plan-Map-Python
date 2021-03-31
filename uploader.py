8f43f2ac677e83027012650bdff745f13a82a5e7
Lindsay Poirier <lindsay.poirier13@gmail.com>
2021-03-31 16:47:18 -0700

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -16,7 +16,7 @@ import ghostscript
 import PyPDF2
 import bcrypt
 from datetime import datetime
-import es 
+import es
 
 
 app = Flask(__name__)                                                                                                                   #create flask object
@@ -48,17 +48,17 @@ blockip = {                                                                     
 }
 @app.route('/')
 def home():                                                                                                                             #function for log in screen
-    
+
     del_list=""                                                                                                                         #list of files in delete section
     for filename in sorted(os.listdir("static/data/places")):
-        if filename.endswith(".txt"):                                                                   
+        if filename.endswith(".txt"):
             filename=filename.replace(".txt","")
             del_list += '<option value="'+filename+'">'+filename+'</option>'                                                            #add each file name in selection list
     del_list=Markup(del_list)                                                                                                           #mark the html script as safe object
     if not session.get('logged_in'):                                                                                                    #if not logged in return login page
         return render_template('login.html')
     else:
-        session['logged_in'] = False                                                        
+        session['logged_in'] = False
         return render_template('upload_index.html',del_list=del_list)                                                                   #if logged in update file list
 
 
@@ -67,20 +67,14 @@ def home():                                                                     
 def do_admin_login():                                                                                                                   #function to collect username password
     global blockip
     if str(request.remote_addr)+"t" in blockip:                                                                                         #check if the ip has exceeded the 10 try mark
-<<<<<<< HEAD
-        if (datetime.now()-blockip[str(request.remote_addr)+"t"]).total_seconds() <20:
-            cal=20-(datetime.now()-blockip[str(request.remote_addr)+"t"]).total_seconds()
-            flash("Try again after "+str(int(cal))+" seconds")
-=======
         if (datetime.now()-blockip[str(request.remote_addr)+"t"]).total_seconds() <1800:
             cal=(1800-(datetime.now()-blockip[str(request.remote_addr)+"t"]).total_seconds())/60
             flash("Try again after "+str(int(cal))+" minutes")
->>>>>>> 9503e217fa1d2cde6d39eefc85bfc3cf1e0f0862
         else:
             flash("Try again ")
             del blockip[str(request.remote_addr)+"t"]
-    else: 
-        
+    else:
+
         filep=open("passw",'r')
         hashed=filep.read().encode('utf-8')
         filep.close()
@@ -91,14 +85,10 @@ def do_admin_login():                                                           
             session['logged_in'] = True
         else:
             if str(request.remote_addr) in blockip:
-                
+
                 blockip[str(request.remote_addr)]+=1
                 if blockip[str(request.remote_addr)]>10:                                                                                    #check if ip address has exceeded 10 incorrect attempts
-<<<<<<< HEAD
-                        msg = Message('Excessive log in attempts', sender = 'generalplanserver@gmail.com', recipients = ['forsomething456@gmail.com'])  #send email for download notification
-=======
                         msg = Message('Excessive log in attempts', sender = 'generalplanserver@gmail.com', recipients = ['ckbrinkley@ucdavis.edu'])  #send email for download notification
->>>>>>> 9503e217fa1d2cde6d39eefc85bfc3cf1e0f0862
                         geoip_data = simple_geoip.get_geoip_data()
                         ip=str(geoip_data['ip'])
                         co=str(geoip_data['location']['country'])
@@ -106,20 +96,16 @@ def do_admin_login():                                                           
                         brow=request.user_agent.platform+" "+request.user_agent.browser
                         msg.body = "Dear Admin,\n\nThere have been excessive log in attempts on Uploader site. The details of the client are as follows:\n\nIP:"+ip+"\nCountry:"+co+"\nRegion:"+rg+"\nBrowser:"+brow+"\n\nGeneral Plan Server."
                         mail.send(msg)
-                        
-<<<<<<< HEAD
-                        flash('Excessive incorrect attempts, Try again after 20 seconds')
-=======
+
                         flash('Excessive incorrect attempts, Try again after 30 minutes')
->>>>>>> 9503e217fa1d2cde6d39eefc85bfc3cf1e0f0862
-                        
+
                         del blockip[str(request.remote_addr)]
                         blockip[str(request.remote_addr)+"t"]=datetime.now()
                 else:
-                    flash('Incorrect Username/Password, please try again')                                                                                            #if ID doesnt match username password                                                         
+                    flash('Incorrect Username/Password, please try again')                                                                                            #if ID doesnt match username password
             else:
                 blockip[str(request.remote_addr)]=0
-                flash('Incorrect Username/Password, please try again') 
+                flash('Incorrect Username/Password, please try again')
     return home()
 
 
@@ -128,14 +114,14 @@ def do_admin_login():                                                           
 def delete_page_update():                                                                                                               #to update page list
     session['logged_in'] = True
     return redirect(url_for('home'))
-    
-@app.route('/delete', methods = ['POST'])                                                        
+
+@app.route('/delete', methods = ['POST'])
 def delete_file():                                                                                                                      #function to delete file from list
     del_req=request.form['deleter']                                                                                                     #access delete button argument
     del_req=del_req+".pdf"
     rempdf = os.path.join("static/data/places", del_req)
     rempdftemp=rempdf.replace("places","temp")
-    remtxt= rempdf.replace(".pdf",".txt")                                                           
+    remtxt= rempdf.replace(".pdf",".txt")
     try:
         os.remove(remtxt)                                                                                                               #remove text file followed by pdf
     except:
@@ -151,21 +137,21 @@ def delete_file():                                                              
 
     drive = GoogleDrive(gauth)                                                                                                          #rebuild the drive object
     top_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()                                                   #generate lsit of files in drive
-    for fileu in top_list:                                                                                                              
+    for fileu in top_list:
         if fileu['originalFilename'] == del_req:                                                                                        #delete file matching the delete request from drive
             fileu.Delete()
 
- 
-    return redirect(url_for('delete_page_update'))
-    
 
-            
+    return redirect(url_for('delete_page_update'))
+
+
+
 
 @app.route('/upload', methods = ['GET', 'POST'])                                                                                        #route to upload form in upload_index html in for getting files and posting to the server
 def upload_file1():                                                                                                                     #function to upload file
     completeName=""
     if request.method == 'POST':                                                                                                        #when upload button is clicked
-        
+
         files = request.files.getlist("file")                                                                                           #get list of files uploaded in form
         for file in files:                                                                                                              #open place pdf file
             print(file.filename)
@@ -176,31 +162,23 @@ def upload_file1():                                                             
                 location_name=request.form['county']
             file.filename=request.form['state']+"_"+request.form['type']+"_"+location_name+"_"+request.form['year']+".pdf"              #generate filename with select form data
             print(file.filename)
-<<<<<<< HEAD
-            msg = Message('General Plan file upload', sender = 'generalplanserver@gmail.com', recipients = ['forsomething456@gmail.com'])  #send email for download notification
-=======
             msg = Message('General Plan file upload', sender = 'generalplanserver@gmail.com', recipients = ['ckbrinkley@ucdavis.edu'])  #send email for download notification
->>>>>>> 9503e217fa1d2cde6d39eefc85bfc3cf1e0f0862
             msg.body = "Dear Admin,\n\nA file named "+file.filename+" has been uploaded to the server by: "+request.form['email']+" .\n\nGeneral Plan Server"
             mail.send(msg)                                                                                                              #send mail for file upload to server
             completeName = os.path.join("static/data/places",file.filename)
 
             print(completeName)
             tempname=os.path.join("static/data/temp",secure_filename(file.filename))                                                    #temporary copy file in case compression is not possible
-            file.save(completeName)                                                                                                     #save file to server         
+            file.save(completeName)                                                                                                     #save file to server
             arg1= '-sOutputFile='+ tempname                                                                                             #path for output file after compression to reduce pdf size
-<<<<<<< HEAD
-            p = subprocess.Popen(['C:\\Program Files\\gs\\gs9.53.1\\bin\\gswin64c.exe',
-=======
             p = subprocess.Popen(['/usr/bin/gs',
->>>>>>> 9503e217fa1d2cde6d39eefc85bfc3cf1e0f0862
                                   '-sDEVICE=pdfwrite','-dCompatibilityLevel=1.4',
                                   '-dPDFSETTINGS=/screen','-dNOPAUSE', '-dBATCH',  '-dQUIET',
                                   str(arg1),completeName ], stdout=subprocess.PIPE)                                                     #function to compress pdf
             try:
                 out, err = p.communicate(timeout=1800)                                                                                  #try compression for 1800s max
             except subprocess.TimeoutExpired:
-                p.kill()                                                                                                                #kill the process since a timeout was triggered                                                                                                   
+                p.kill()                                                                                                                #kill the process since a timeout was triggered
                 out, error = p.communicate()                                                                                            #capture both standard output and standard error
             else:
                 pass
@@ -215,7 +193,7 @@ def upload_file1():                                                             
                 os.remove(tempname)                                                                                                     #remove temp file if compressed pdf is corrupt and causes exception
             else:
                 pass
-                os.remove(completeName) 
+                os.remove(completeName)
                 shutil.move(tempname,"static/data/places")                                                                              #move compressed tempfile to places directory is compressed file is valid
             fname =completeName
             fnamecpy=fname
@@ -226,19 +204,14 @@ def upload_file1():                                                             
                 if not page.getText():                                                                                                  #check check if pdf contains scanned data or text
                     imornot=imornot+1
 
-            if imornot > int(length/2):                                                                                                 #if more than half pages of pdf are scanned convert to text pdf through OCR  
-<<<<<<< HEAD
-                fname=fname.replace('.pdf', '')                                     
-                textfile = open(fname + ".txt", "a")                                                                                    #create text file with place name
-=======
+            if imornot > int(length/2):                                                                                                 #if more than half pages of pdf are scanned convert to text pdf through OCR
                 fname=fname.replace('.pdf', '')
-                text_file_name = fname + ".txt"                                 
+                text_file_name = fname + ".txt"
                 textfile = open(text_file_name, "a")                                                                                    #create text file with place name
->>>>>>> 9503e217fa1d2cde6d39eefc85bfc3cf1e0f0862
-                for page in doc:                                                                        
+                for page in doc:
                     pix = page.getPixmap(alpha = False)                                                                                 #generate image file from page
-                    pixn=os.path.join("static/data/places","page-%i.png" % page.number)     
-                    pix.writePNG(pixn)                                                                                                  #save page image as png 
+                    pixn=os.path.join("static/data/places","page-%i.png" % page.number)
+                    pix.writePNG(pixn)                                                                                                  #save page image as png
                     pdfn=os.path.join("static/data/places","page-"+str(page.number)+".pdf")
                     with open(pdfn, 'w+b') as f:
                         text = pytesseract.image_to_string(pixn)                                                                        #obtain text from image
@@ -255,23 +228,23 @@ def upload_file1():                                                             
                     pdfn=os.path.join("static/data/places",("page-"+str(fileNumber)+".pdf"))
                     mergedObject.append(PdfFileReader(pdfn, 'rb'))                                                                      #append page to place pdf
                     os.remove(pdfn)                                                                                                     #remove appended page pdf from server
-                 
+
                 mergedObject.write(fnamecpy)                                                                                            #save the complete place pdf to single file in server
-                
+
                 mergedObject.close()
-            
+
 
             else:                                                                                                                       #if the pdf contains less than hald scanned pages
                 imornot=0
-                fname=fname.replace('.pdf', '')             
+                fname=fname.replace('.pdf', '')
                 textfile = open(fname + ".txt", "wb")                                                                                   #create text document to store text data
                 for page in doc:
                         text = page.getText().encode("utf8")                                                                            #get text from pdf page
-                        textfile.write(text)                                                                                            #write text to text file for the place 
+                        textfile.write(text)                                                                                            #write text to text file for the place
                         textfile.write(bytes((12,)))
                 textfile.close()
-              
-            doc.close()         
+
+            doc.close()
 
         drive = GoogleDrive(gauth)                                                                                                      #rebuild drive object
         file1=drive.CreateFile({'title':file.filename})                                                                                 #name the drive file
@@ -280,19 +253,13 @@ def upload_file1():                                                             
 
 
     up="Files Uploaded Successfully!"
-    
-<<<<<<< HEAD
-=======
+
+
     es.add_to_index(text_file_name)
->>>>>>> 9503e217fa1d2cde6d39eefc85bfc3cf1e0f0862
-    
+
     return render_template('upload_confirm.html',up=up)                                                                                 #render upload confirmation message page
 
 
-if __name__ == "__main__":                                                                                                              #run app on local host at port 5001 in debug mode                                                                                      
+if __name__ == "__main__":                                                                                                              #run app on local host at port 5001 in debug mode
     app.secret_key = os.urandom(12)                                                                                                     #random key for log in authentication
-    app.run(host="0.0.0.0", port=5001, debug=True)                          
-     
-
-
-
+    app.run(host="0.0.0.0", port=5001, debug=True)
